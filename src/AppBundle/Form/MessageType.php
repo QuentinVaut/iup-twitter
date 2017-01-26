80a9617cae934dbe304b28841232e8461ebf5774
Quentin <quentin-vauthier@hotmail.fr>
2017-01-26 14:21:26 +0100

    TexteArea + Nettoyage template

--- src/AppBundle/Form/MessageType.php
+++ src/AppBundle/Form/MessageType.php
@@ -4,6 +4,7 @@ namespace AppBundle\Form;
 
 use AppBundle\Entity\Message;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
@@ -17,7 +18,9 @@ class MessageType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('content')
+            ->add('content', TextareaType::class , array(
+                'attr' => array('cols' => '50', 'rows' => '5'),
+            ))
             ->add('picture', FileType::class, array(
                 'label' => 'Picture (PNG, JPG, GIF, MP4, PDF)',
                 'data_class' => null,
@@ -35,4 +38,3 @@ class MessageType extends AbstractType
         ));
     }
 }
- 
